0968ef61012ca6f4c5443e2af16565d615f458ca
gusadolfo123 <gusadolfo123@hotmail.com>
2018-12-06 12:50:21 -0500

    Proyecot Inicial con Redux

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,27 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+import {BrowserModule} from '@angular/platform-browser';
+import {NgModule} from '@angular/core';
 
-import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
+import {AppRoutingModule} from './app-routing.module';
+import {AppComponent} from './app.component';
+import {FooterComponent} from './footer/footer.component';
+import {TodoComponent} from './todo/todo.component';
+import {TodosListComponent} from './todo/todos-list/todos-list.component';
+import {TodoFooterComponent} from './todo/todo-footer/todo-footer.component';
+import {TodoItemComponent} from './todo/todo-item/todo-item.component';
+import {TodoAddComponent} from './todo/todo-add/todo-add.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    FooterComponent,
+    TodoComponent,
+    TodosListComponent,
+    TodoFooterComponent,
+    TodoItemComponent,
+    TodoAddComponent,
   ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule
-  ],
+  imports: [BrowserModule, AppRoutingModule],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
